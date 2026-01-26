c345d97f1aa6bfb1f7fe3031b0fedb93408a50bd
gamza <aprille138@gmail.com>
2026-01-26 13:10:50 +0900

    feat: files for pwa

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onNeedRefresh() {
+    if (confirm("새 버전이 있어요. 새로고침할까요?")) {
+      window.location.reload();
+    }
+  },
+  onofflineReady() {
+    console.log("offline ready");
+  },
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: "generateSW",
+      filename: "sw.js",
+      registerType: "prompt",
+      devOptions: {
+        enabled: false,
+      },
+
+      manifest: {
+        name: "BreadFeet",
+        short_name: "BreadFeet",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
+          { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+    }),
+  ],
+});
