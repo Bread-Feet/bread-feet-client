abce88768d3c3f9c11188760b1f635c9a58b8096
gamzazoha <aprille138@gmail.com>
2026-02-22 15:20:07 +0900

    fix: pagination stop

--- src/pages/bakery/hooks/useBakeries.js
+++ src/pages/bakery/hooks/useBakeries.js
@@ -30,6 +30,7 @@ export default function useBakeries(keyword) {
       }
     } catch (err) {
       console.error(err);
+      setHasMore(false);
     } finally {
       setIsLoading(false);
     }
